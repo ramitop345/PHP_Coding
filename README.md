b80ff2e9b1d8484958ee5aeecb4b0f85c154382f
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-09-29 13:54:13 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PHP_Coding
 PHP Basics and complex syntaxes
+
+# add the php exe path contained in xamp path in the phpactivatepath in vsc
+
+# working with php in vsc
+- extensions: php inteliphense, live server , php server
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "I love Pizza";
+echo "it is really good";
+// this is a comment
+/*this is 
+a multiline comment */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Code</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    
+</body>
+</html>
