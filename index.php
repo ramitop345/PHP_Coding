12273e2b2c09379fb1fccc81855625bb43ded0e0
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-09-30 15:30:26 +0200

    gobal variable  to access all headers, paths and connection informations for my current webpage

--- index.php
+++ index.php
@@ -1,47 +1,36 @@
 <?php
-session_start();
+
+// The array contains all headers, paths and scripts in the current web_page. 
+//we can use it for example to access file name dynamically so that we can change 
+//the original name without needing to do that in the other files where the actual file is called 
+    //foreach($_SERVER as $key => $value){
+      //  echo "{$key} = {$value} <br>";
+    //}
+
+    //checking post method status with §_SERVER
+    // old callling: if(isset($_POST["login]))
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        echo "Hello";
+    }
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <form action="index.php" method="post">
-        <label>username:</label><br>
-        <input type="text" name="username"> <br>
-        <label>password:</label> <br>
-        <input type="password" name="password"> <br>
-        <input type="submit" value="login" name="login"> <br>
+    <!--<form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">-->
+    <!-- The htmlspecialchars() helps encode special characters for html -->
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <label for="">username: </label> <br>
+    <input type="text" name="username">
+    <input type="submit" value= "login" name="login">
+
     </form>
 </body>
-
 </html>
-
-<?php
-// the method $_SESSION is used to save data in the global data that can be recalled in different files
-//when we call the session_start function in the file
-
-//header() function is used to jump to a specific file, it can be called when we need after login
-//to escape to a specific page
-if (isset($_POST["login"])) {
-    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
-
-        $_SESSION["username"] = $_POST["username"];
-        $_SESSION["password"] = $_POST["password"];
-
-        header("Location: home.php");
-
-        
-    }
-
-    else echo "Username or Password missing";
-}
-
-?>
