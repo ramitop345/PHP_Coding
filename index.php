2c44bed44e9325a78a62bfec08b541778e068dbf
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-09-30 15:02:45 +0200

    creating a session and storing informations like username or password to be reused in other files(session_start, session_destroy, , header()

--- index.php
+++ index.php
@@ -1,20 +1,47 @@
-
 <?php
-    include("header.html")
+session_start();
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    This is the Home Page <br>
-    Enter all stuff about your home page here
+    <form action="index.php" method="post">
+        <label>username:</label><br>
+        <input type="text" name="username"> <br>
+        <label>password:</label> <br>
+        <input type="password" name="password"> <br>
+        <input type="submit" value="login" name="login"> <br>
+    </form>
 </body>
+
 </html>
+
 <?php
-    include("footer.html")
+// the method $_SESSION is used to save data in the global data that can be recalled in different files
+//when we call the session_start function in the file
+
+//header() function is used to jump to a specific file, it can be called when we need after login
+//to escape to a specific page
+if (isset($_POST["login"])) {
+    if (!empty($_POST["username"]) && !empty($_POST["password"])) {
+
+        $_SESSION["username"] = $_POST["username"];
+        $_SESSION["password"] = $_POST["password"];
+
+        header("Location: home.php");
+
+        
+    }
+
+    else echo "Username or Password missing";
+}
+
 ?>
